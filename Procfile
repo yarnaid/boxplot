dea7aeb7ba8c12649ada5a2d1d4a33bb2ec92217
yarnaid <yarnaid@gmail.com>
2015-03-25 21:07:40 +0300

    files added

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn awo8:app
+worker:
---
+++ awo8.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+
+import json
+import os
+import data_parser
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/ajax_get_data', methods=["GET", "POST"])
+def ajax_get_answers():
+    if request.method == 'POST':
+        df = pd.read_json(os.path.join(app.root_path, './static/data/sample.json'))
+        data = data_parser.parse(df)
+
+        res = jsonify(result=data)
+        return res
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run()
